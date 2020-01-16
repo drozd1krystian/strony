ddef45dca087b8026642b2c404974c079f457d0e
Krystian <drozd1krystian@gmail.com>
2020-01-16 18:32:20 +0100

    poprawki

--- salon_fryzjerski/dist/js/main.js
+++ salon_fryzjerski/dist/js/main.js
@@ -34,7 +34,7 @@ $('#navbar a, .btn').on('click', function(event) {
 const navSlide = () => {
   const burger = document.querySelector('.burger');
   const nav = document.querySelector('.links');
-
+  
   burger.addEventListener('click', () => {
     nav.classList.toggle('active');
     burger.classList.toggle('toggle');
