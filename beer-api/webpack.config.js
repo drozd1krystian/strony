578b9c98dbb840e72686cc86b40e0486f670998f
Krystian <drozd1krystian@gmail.com>
2020-01-18 19:31:31 +0100

    wyglad

---
+++ beer-api/webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+module.export = {
+    entry: {
+        app: './src/index.js'
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'index.bundle.js'
+    },
+    module: {
+        rules: [{
+            test: /\.js?$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader',
+            query: {
+                presets: ['@babel/preset-env']
+            }
+        }]
+    }
+}
