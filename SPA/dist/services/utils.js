db968ea82e771f4c97a8a95e19ecc40719b4b05f
Krystian <drozd1krystian@gmail.com>
2020-01-31 15:25:25 +0100

    poprawki

--- SPA/dist/services/utils.js
+++ SPA/dist/services/utils.js
@@ -4,12 +4,10 @@ const Utils = {
     let r = url.split("/");
     let request = {
       resource: null,
-      id: null,
       verb: null
     };
     request.resource = r[1];
-    request.id = r[2];
-    request.verb = r[3];
+    request.verb = r[2];
     return request;
   },
   sleep: ms => {
