c7840a96a21a0a40c19a318bbddcf3d7052f8cb3
Krystian <drozd1krystian@gmail.com>
2020-02-02 22:03:06 +0100

    bledy w menu

--- kawiarnia/dist/js/main.js
+++ kawiarnia/dist/js/main.js
@@ -1,50 +1,49 @@
 // Smooth Scrolling
-$('#navbar a, .btn').on('click', function(event) {
-    if(this.hash !== '') {
-      event.preventDefault();
-      
-      const hash = this.hash;
-      function detectmob() {
-        if(window.innerWidth <= 800 && window.innerHeight <= 600) {
-          return true;
-        } else {
-          return false;
-        }
-     }
-      if(detectmob()) {
-      $('html, body').animate(
-        {
-          scrollTop: $(hash).offset().top - 100,
-        },
-        800
-      );
-     }
-     else {
-      $('html, body').animate(
+$("#navbar a, .btn").on("click", function(event) {
+  if (this.hash !== "") {
+    event.preventDefault();
+
+    const hash = this.hash;
+    function detectmob() {
+      if (window.innerWidth <= 800 && window.innerHeight <= 600) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+    if (detectmob()) {
+      $("html, body").animate(
         {
           scrollTop: $(hash).offset().top - 100
         },
         800
       );
-     }   
-    } 
+    } else {
+      $("html, body").animate(
+        {
+          scrollTop: $(hash).offset().top - 100
+        },
+        800
+      );
+    }
+  }
+});
+
+// Burger Menu
+const navSlide = () => {
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".links");
+  const links = document.querySelectorAll(".links li a");
+  burger.addEventListener("click", () => {
+    nav.classList.toggle("active");
+    burger.classList.toggle("toggle");
   });
-  
-  // Burger Menu
-  const navSlide = () => {
-    const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.links');
-    const links = document.querySelectorAll('.links li a');
-
-    burger.addEventListener('click', () => {
-      nav.classList.toggle('active');
-      burger.classList.toggle('toggle');
-    })
-
-    links.forEach(el => el.onclick = () => {
-      nav.classList.toogle('active');
-      burger.classList.toogle('toogle');
-    })
-    
-  };
-  navSlide();
+  Array.from(links).forEach(
+    el =>
+      (el.onclick = () => {
+        nav.classList.toggle("active");
+        burger.classList.toggle("toggle");
+      })
+  );
+};
+navSlide();
